34e076df94399900559153b82b6f343be2c85897
anurag-shukla8004 <79192104+anurag-shukla8004@users.noreply.github.com>
2021-05-02 15:45:12 +0530

    Add files via upload

--- Add_book_project-1/app.js
+++ Add_book_project-1/app.js
@@ -25,6 +25,47 @@ row.innerHTML = `
 list.appendChild(row);
 }
 
+//show alert
+UI.prototype.showAlert = function(message, className){
+  //Creat div
+  const div = document.createElement('div');
+  //add classes
+  div.className = `alert ${className}`;
+
+  //add text
+  div.appendChild(document.createTextNode(message));
+
+  //get parent
+
+  const container = document.querySelector('.container');
+
+  //get form
+  const form = document.querySelector('#book-form');
+// Insert alert
+  container.insertBefore(div, form);
+
+  // Timeout after 3 sec
+  setTimeout(function(){
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
+// Deleted book 
+
+UI.prototype.deleteBook= function(target) {
+  if(target.className ==='delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
+
+
+// Clear Fields
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}
 
 
 // Event Listeners
@@ -41,10 +82,38 @@ const book = new Book(title, author, isbn);
 // Instantiate UI
 const ui = new UI();
 
+// validation
+if(title === '' || author === '' || isbn === ''){
+  ui.showAlert('please fill in all fields', 'error');
+}else {
+  
+
+
 // Add book to list
 ui.addBookToList(book);
 
-console.log(book);
+ // Show success
+ ui.showAlert('Book Added!', 'success');
+
+// Clear fields
+ui.clearFields();
+}
+ 
 
   e.preventDefault();
 });
+
+// Event Lsitnerat for delete
+
+document.getElementById('book-list').addEventListener('click', function(e){
+
+ const ui = new UI(); 
+
+ //Delete book
+ ui.deleteBook(e.target);
+
+ //show message 
+ ui.showAlert('Book removed!', 'success');
+
+  e.preventDefault();
+})
